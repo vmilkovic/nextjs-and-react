b51e1d1c894e842cbf4322926904f790c8facc87
Vedran Milković <vedran.milkovic25@gmail.com>
2021-07-11 18:44:27 +0200

    Pages & File-based Routing

---
+++ nextjs/pages/clients/[id]/[clientprojectid].js
@@ -0,0 +1,9 @@
+function SelectedClientProjectPage() {
+  return (
+    <div>
+      <h1>The Project Page for a Specific Project for a Selected Client</h1>
+    </div>
+  );
+}
+
+export default SelectedClientProjectPage;
---
+++ nextjs/pages/clients/[id]/index.js
@@ -0,0 +1,23 @@
+import { useRouter } from "next/router";
+
+function ClientProjectsPage() {
+  const router = useRouter();
+
+  console.log(router.query);
+
+  function loadProjectHandler() {
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "max", clientprojectid: "projecta" },
+    });
+  }
+
+  return (
+    <div>
+      <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
+    </div>
+  );
+}
+
+export default ClientProjectsPage;
