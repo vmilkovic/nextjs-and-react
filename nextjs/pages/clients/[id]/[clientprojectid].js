d0946d3a1bea90384dd4a1b4ede82746994e3ac8
Vedran Milković <vedran.milkovic25@gmail.com>
2021-07-13 20:15:58 +0200

    Delete initial app

--- nextjs/pages/clients/[id]/[clientprojectid].js
+++
@@ -1,9 +0,0 @@
-function SelectedClientProjectPage() {
-  return (
-    <div>
-      <h1>The Project Page for a Specific Project for a Selected Client</h1>
-    </div>
-  );
-}
-
-export default SelectedClientProjectPage;
